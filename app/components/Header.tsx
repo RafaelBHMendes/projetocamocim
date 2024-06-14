5edc73aa3926ea644ff033c10b2057eed24a3425
Pendragon21 <rafaelbemvindo@hotmail.com>
2024-06-13 22:18:01 -0300

    "fix: deatures"

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,25 +1,39 @@
 import Image from "next/image";
 import LogoSAAE from "../../public/LogoSAAE.jpeg";
+import atendimento from "../../public/atendimento.png";
 
 const Header: React.FC = () => {
   return (
-    <header className="bg-blue-700 text-white shadow-md">
-      <div className="container mx-auto p-4 px-28 flex justify-between items-center">
-        <div className="flex items-center">
+    <header>
+      <div className="bg-blue-700 text-white shadow-md">
+        <div className="container mx-auto p-4 flex justify-between items-center">
           <Image
             src={LogoSAAE}
             alt="Logo SAAE"
-            height={200}
-            width={200}
+            height={180}
+            width={180}
             className="rounded-md"
           />
+          <span className="mr-12 text-2xl font-bold">
+            Serviço Autônomo de Água e Esgoto de Camocim
+          </span>
         </div>
-
-        <span className="text-2xl font-bold">
-          Sistema Autônomo de Água e Esgoto de Camocim
-        </span>
       </div>
-      <nav className="bg-blue-900">
+      <div className="bg-blue-900 text-white text-center py-8">
+        <a
+          href="https://www.saaecamocimce.com.br/#/agencia/inicio"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            src={atendimento}
+            alt="Portal de Serviços Autoatendimento"
+            className="mx-auto cursor-pointer"
+            layout="responsive"
+          />
+        </a>
+      </div>
+      <nav className="bg-blue-700">
         <div className="container mx-auto p-4 flex justify-center items-center space-x-6 font-semibold text-sm md:text-base">
           <a href="/" className="hover:text-blue-200 transition-colors">
             Início
